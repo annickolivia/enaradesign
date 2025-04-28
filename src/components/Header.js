e668bf71b48957417210f167e18aaa685a6dcab7
Jonathan <jonathanandrinantenaina@gmail.com>
2025-04-28 22:42:26 +0300

    navbar done(no animation)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './navbar';
+import '../styles/header.scss';
 
 function Header({children}) {
   return (
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
+import '../styles/navbar.scss'
+import { Link } from 'react-router-dom';
+
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const Portfolio = [
+    "Architecture", 
+    "Design d'Interieur", 
+    "Mobilier", 
+    "2D Plan concept",
+    "Animation architecturale",
+    "Réalité Virtuel"
+  ]
+
+  const partenaires = [
+    "Hay Tao Home",
+    "Enara Construction",
+    "Autres"
+  ]
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
   return (
-    <nav className="bg-white shadow-lg">
-      <div className="max-w-6xl mx-auto px-4">
+    <nav className={`${isOpen ? 'bg-black' : 'bg-transparent'}`}>
+      <div className="max-w-full mx-5 px-10 sm:px-20">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <span className="text-4xl font-bold text-gray-800">Logo</span>
+            <span className={`text-2xl font-bold ${isOpen ? 'text-white':' text-gray-800'}`}>Enara.</span>
           </div>
 
           {/* Mobile menu button */}
@@ -52,33 +69,86 @@ function Navbar() {
       </div>
 
       {/* Mobile Menu */}
-      <div className={`${isOpen ? 'block' : 'hidden'}`}>
-        <div className="px-8 pt-2 pb-3 space-y-1 sm:px-3 bg-gray-50 h-screen bg-black ">
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-4xl font-medium text-white hover:text-indigo-600 hover:bg-gray-100 transition duration-300"
+      <div className={`flex flex-row bg-black items-center justify-center ${isOpen ? 'block' : 'hidden'}`}>
+
+        <div className="pt-2 pb-3 pr-32 space-y-1 mr-28 px-3 h-screen flex flex-col justify-center md:border-r-gray-900 md:border-r-2
+          
+        ">
+          <Link
+            to="/accueil"
+            className="pages px-4 py-2 rounded-md text-4xl font-medium text-white transition duration-300"
           >
             Accueil
-          </a>
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-4xl font-medium text-white hover:text-indigo-600 hover:bg-gray-100 transition duration-300"
+          </Link>
+          <Link
+            to="/portfolio"
+            className="pages px-4 py-2 rounded-md text-4xl font-medium text-white  transition duration-300"
           >
             Portfolio
-          </a>
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-4xl font-medium text-white hover:text-indigo-600 hover:bg-gray-100 transition duration-300"
+          </Link>
+          <Link
+            to="/services"
+            className="pages px-4 py-2 rounded-md text-4xl font-medium text-white transition duration-300"
           >
             Services
-          </a>
-          <a
-            href="#"
-            className="block px-3 py-2 rounded-md text-4xl font-medium text-white hover:text-indigo-600 hover:bg-gray-100 transition duration-300"
+          </Link>
+          <Link
+            to="/articles"
+            className="pages px-4 py-2 rounded-md text-4xl font-medium text-white transition duration-300"
           >
-            Blog
-          </a>
+            Articles
+          </Link>
         </div>
+        {/* droiiteeee ---------------*/}
+        <div className='flex-col droite h-screen justify-center align-items-center hidden md:flex'>
+          <div className="droite-haut flex md:border-b-gray-800 md:border-b-2 md:py-10">
+            {/* Portfolio */}
+            <div>
+              <h6 className="text-white">Porfolio</h6>
+              {Portfolio.map((item, index) => {
+                  return (
+                    <Link
+                      key={index}
+                     to={`/${item.toLowerCase()}`}
+                      className="block px-4 py-2 rounded-md text-4xl font-medium text-white transition duration-300 sous-link"
+                    >
+                      {item}
+                    </Link>
+                  );
+              })}
+            </div>
+            <div>
+              <h6 className="text-white">Nos Partenaires</h6>
+                {partenaires.map((item, index) => {
+                  return (
+                    <Link
+                      key={index}
+                     to={`/${item.toLowerCase()}`}
+                      className="block px-4 py-2 rounded-md text-4xl font-medium text-white transition duration-300 sous-link"
+                    >
+                      {item}
+                    </Link>
+                  );
+                })}
+            </div>
+          </div>
+          <div className='droite-bas flex sous-link mt-10'>
+                <div>
+                  <h6 className="text-white">Madagascar</h6>
+                  <p>Lot Xxxxxxxx</p>
+                  <p>Antsakaviro</p>
+                  <p>Antananarivo</p>
+                  <p>+261 32 05 802 56</p>
+                </div>
+                <div>
+                  <h6 className='text-white'>International</h6>
+
+                  <p>xxxxxxxxxxxxx</p>
+                  <p>+31 174 705 811</p>
+                </div>
+          </div>
+        </div>
+
       </div>
     </nav>
   );
